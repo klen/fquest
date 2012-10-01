337bff8c8ad67860ec4ab6e84e4aacede087afb5
Kirill Klenov <horneds@gmail.com>
2012-10-01 17:42:49 +0400

    working

--- base/fquest/views.py
+++ base/fquest/views.py
@@ -46,7 +46,7 @@ def profile():
         .order_by(Event.created_at.desc()).paginate(page, per_page=20)
     return render_template(
         'fquest/profile.html',
-        title="%s — %s" % (character.name, character.display()),
+        title=u"%s - %s" % (character.name, character.display()),
         events=events,
         character=character)
 
@@ -59,7 +59,7 @@ def character(facebook_id):
         .order_by(Event.created_at.desc()).paginate(page, per_page=20)
     return render_template(
         'fquest/profile.html',
-        title="%s — %s" % (character.name, character.display()),
+        title=u"%s - %s" % (character.name, character.display()),
         events=events,
         character=character)
 
