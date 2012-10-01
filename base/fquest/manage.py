879eb2d2eb8ba2022f46da488588f8cbb7083048
Kirill Klenov <horneds@gmail.com>
2012-10-01 16:51:21 +0400

    working

--- base/fquest/manage.py
+++ base/fquest/manage.py
@@ -9,3 +9,13 @@ def create_world():
 
     GEN_MONSTERS(num=1000)
     GEN_STUFF(num=1000)
+
+
+@manager.command
+def give_exp(facebook_id, exp):
+    " This is cheat. "
+    from .models import Character
+
+    character = Character.query.filter(facebook_id=facebook_id).first()
+    if character:
+        character.got_exp(int(exp))
