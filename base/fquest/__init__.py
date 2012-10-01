081b294a4c0f973a752444560b9d37ee7a2c65b2
Kirill Klenov <horneds@gmail.com>
2012-10-01 11:52:21 +0400

    Added some world generation

--- base/fquest/__init__.py
+++ base/fquest/__init__.py
@@ -2,5 +2,11 @@
 
 
 def loader_meta(app):
+    " Init fquest stuff. "
+
     from .views import fquest
     app.register_blueprint(fquest)
+
+    from .models import *
+
+# pymode:lint_ignore=W404
--- base/fquest/tests.py
+++ base/fquest/tests.py
@@ -1,4 +1,5 @@
 from ..core.tests import FlaskTest
+from .generator import GEN_MONSTERS, GEN_STUFF
 
 
 class TestCase(FlaskTest):
@@ -6,5 +7,20 @@ class TestCase(FlaskTest):
     def setUp(self):
         super(TestCase, self).setUp()
 
-    def test_character(self):
-        pass
+        # Create test world
+        GEN_MONSTERS(num=100, max_level=20)
+        GEN_STUFF(num=100, max_level=20)
+
+    def test_monster(self):
+        from .models import Monster
+        from sqlalchemy import func
+
+        monster = Monster.query.order_by(func.random()).first()
+        self.assertTrue(monster)
+
+    def test_stuff(self):
+        from .models import Stuff
+        from sqlalchemy import func
+
+        stuff = Stuff.query.order_by(func.random()).first()
+        self.assertTrue(stuff)
